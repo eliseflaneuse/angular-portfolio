42fda2f429ae16c516cf232e30ad9d76100c0ba6
Elise Flaneuse <eliseflaneuse@gmail.com>
2024-05-07 14:27:22 -0300

    Texts for work experiences

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -17,14 +17,14 @@ export class ExperiencesComponent {
         strong: 'Fullstack Developer',
         p: 'IBM | Aug 23 - Present',
       },
-      text: '<p>Text</p>',
+      text: '<p>I developed and maintained applications using TypeScript and Angular, seamlessly integrated Microsoft Azure services, collaborated with cross-functional teams, demonstrated strong problem-solving skills, adapted swiftly to dynamic environments, and committed to continuous learning and incorporating emerging technologies, with a focus on TDD and CI/CD practices in an Angular and TypeScript environment.</p>',
     },
     {
       summary: {
         strong: 'Front-end Developer',
         p: 'Flaneuse Studios | Jun 23 - Present',
       },
-      text: '<p>Text</p>',
+      text: '<p>Full-stack developer specializing in website development. I provide services ranging from conception and design to implementation and maintenance of web projects. My experience includes using various front-end and back-end technologies to create efficient and customized digital solutions for my clients.</p>',
     },
   
   ]);
